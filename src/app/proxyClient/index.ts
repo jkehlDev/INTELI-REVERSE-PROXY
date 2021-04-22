130ea299c00f2aa6808dac5bb3d9edd634916cfb
ThibaudGLT <guillotthibaud@gmail.com>
2021-04-22 10:20:09 +0200

    changes into typescript for ProxyClient

---
+++ src/app/proxyClient/index.ts
@@ -0,0 +1,97 @@
+// Imports externals modules
+import { connection, client as WsSocketClient } from 'websocket';
+import ActionEnum from '../protocol/enums/ActionEnum';
+import TypeEnum from '../protocol/enums/TypeEnum';
+import ProxyEvent from '../protocol/ProxyEvent';
+import clientConfig from './clientConfig.json';
+
+/**
+* @class ProxyClient - This Class referred to the wsClient not the Final Client (Browser)
+* @version 1.00
+*/
+
+class ProxyClient {
+  private wsClient:WsSocketClient = new WsSocketClient();
+  private domain: string;
+  private port: number;
+  private connection:connection;
+
+  /**
+   * @constructor This provide instance of Inteli-proxy WS Client connection
+   * @param domain - Strictly your WS Client domain - default value as an example
+   * @param port - Strictly your WS Client port - default value as an example
+   */
+
+  constructor(domain:string = "my.domain.com", port:number = 3080) {
+    this.domain= domain;
+    this.port= port;
+    this.wsClient.on("connectFailed", this.wsClientConnectFailedHandler);
+    this.wsClient.on("connect", this.wsClientConnectHandler);
+}
+
+  /**
+  * @method ProxyClient#Open Attempt to open connection to the server
+  */
+
+   open(){
+    this.wsClient.connect(`ws://${this.domain}/${this.port}`)
+  }
+
+  /**
+  * @method ProxyClient#Close Attempt to close connection to the server
+  */
+
+  close(){
+    if (this.connection) {
+      this.connection.close();
+      console.log("Client want to close");
+    }
+  }
+
+  /**
+   * @method ProxyClient#wsClientConnectFailedHandler Deal with error from the connection attempt
+   */
+
+   wsClientConnectFailedHandler(error:Error) {
+    console.error(error);
+    throw error;
+  }
+
+  /**
+  * @method ProxyClient#wsClientConnectHandler That will occured if connection succeeds
+  * @param connection WS Client connection object
+  */
+
+  wsClientConnectHandler(connection:connection) {
+    console.log("WebSocket Client Connected");
+    connection.on("error", function (error) {
+        console.error(error);
+      });
+      connection.on("close", function () {
+        console.log("Connection Closed by Server");
+        this.connection = null;
+      });
+      connection.on("message", function (message) {
+        if (message.type === "utf8") {
+          console.log("Received: '" + message.utf8Data + "'");
+        }
+      });
+      this.connection = connection;
+      const openProxyEvent : ProxyEvent = {
+        header: {
+          type: TypeEnum.proxy,
+          action: ActionEnum.open,
+        },
+        authentification: {
+            token:"",
+        },
+        payload: clientConfig
+    };      
+
+      connection.send(JSON.stringify(openProxyEvent));
+     
+}
+
+}
+
+export default ProxyClient;
