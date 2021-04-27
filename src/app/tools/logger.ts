446879952cf706d029b7cc0a0869260b26e9488b
ThibaudGLT <guillotthibaud@gmail.com>
2021-04-27 17:51:52 +0200

    [WIP Logger] Trying to display the filname which called the log

--- src/app/tools/logger.ts
+++ src/app/tools/logger.ts
@@ -1,24 +1,22 @@
 import { createLogger, format, transports } from 'winston';
 
-const myMessage = format.printf(({ level, message, label, timestamp }) => {
-  return `${timestamp} [${label}] ${level}: ${message}`;
-});
-
 const logger = createLogger({
   level: 'info',
   format: format.combine(
-    format.label({ label: 'init' }),
     format.timestamp(),
+    format.label({label : `filename`}),
     format.colorize(),
-    myMessage
-  ),
-
-  transports: [
-    new transports.File({ filename: 'error.log', level: 'error' }),
-    new transports.File({ filename: 'combined.log' }),
-  ],
-});
-
+    ),
+    
+    transports: [
+      new transports.File({ filename: 'error.log', level: 'error' }),
+      new transports.File({ filename: 'combined.log' }),
+    ],
+  });
+  
+  const myMessage = format.printf(({ timestamp, level, label, message}) => {
+    return `${timestamp} ${level}: [${label}] ${message}`;
+  });
 //
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
@@ -26,9 +24,9 @@ const logger = createLogger({
 if (process.env.NODE_ENV !== 'production') {
   logger.add(
     new transports.Console({
-      format: format.simple(),
+        format: myMessage,
     })
-  );
+  )
 }
 
 export default logger;
